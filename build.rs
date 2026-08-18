702846aa6ef89fe5d856e113ced0d28630e0c796
Mohammadreza Asadollahifard <mazafard@gmail.com>
2026-08-18 04:32:56 +0100

    feat(windows): disable console terminal window and embed native taskbar icon.ico

--- build.rs
+++ build.rs
@@ -1,3 +1,13 @@
 fn main() {
     slint_build::compile("ui/appwindow.slint").expect("Slint build failed");
+
+    #[cfg(target_os = "windows")]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("assets/icon.ico");
+        res.set("ProductName", "APDL");
+        res.set("FileDescription", "Aparat Video & Playlist Downloader");
+        res.set("LegalCopyright", "Copyright (c) Mohammadreza A. Fard");
+        let _ = res.compile();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     ap_dl::run_app()
